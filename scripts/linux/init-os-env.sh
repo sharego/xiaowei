1ae9b8bb209cfe00d4624ebdfd83b0f8dd7b18f0
sharego <xw_cht.y@live.cn>
2019-05-06 22:58:53 +0800

    Update init-os-env.sh
    add docker-compose

--- scripts/linux/init-os-env.sh
+++ scripts/linux/init-os-env.sh
@@ -39,7 +39,9 @@ cat > /etc/docker/daemon.json << EOF
 }
 EOF
 
-
+# add docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod a+x /usr/local/bin/docker-compose
 
 # use zsh & oh-my-zsh
 yum -y install zsh
