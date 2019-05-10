d63d3beb6b280b40a5ab0d99e76dd71e29412a34
sharego <xw_cht.y@live.cn>
2019-05-10 13:12:09 +0800

    Update init-os-env.sh
    add history size increase

--- scripts/linux/init-os-env.sh
+++ scripts/linux/init-os-env.sh
@@ -43,6 +43,9 @@ EOF
 curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod a+x /usr/local/bin/docker-compose
 
+# 个人电脑多保存一些 history
+sed -i 's/HISTSIZE=1000/HISTSIZE=10000/g' /etc/profile
+
 # use zsh & oh-my-zsh
 yum -y install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
