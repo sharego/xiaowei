46f39d42163253e839c07f1929498475cbb2e72f
xiaowei <xw927@outlook.com>
2019-09-25 19:37:56 +0800

    update init-os

--- scripts/linux/init-os-env.sh
+++ scripts/linux/init-os-env.sh
@@ -1,6 +1,7 @@
 yum install -y yum-utils
 yum -y install epel-release
-yum -y install wget  nmap-ncat psmisc net-tools bind-utils curl python-setuptools python2-pip unzip python36-devel python36-pip
+yum -y install wget bash-completion bash-completion-extras nmap-ncat psmisc net-tools bind-utils curl python-setuptools python2-pip unzip python36-devel python36-pip
+
 wget -O /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod a+x /usr/local/bin/jq
 yum makecache fast
 
@@ -14,11 +15,14 @@ yum-config-manager --add-repo http://download.docker.com/linux/centos/docker-ce.
 # wget -O /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo
 # sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
 
-mkdir ~/.config/pip /etc/docker
+
 systemctl stop firewalld
 systemctl disable firewalld
 setenforce 0
 sed  -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+
+mkdir -p ~/.config/pip /etc/docker
 
 cat >> /etc/sysct.conf << EOF
 net.ipv4.ip_forward = 1
@@ -37,7 +41,7 @@ cat > /etc/docker/daemon.json << EOF
 {
     "storage-driver": "overlay2",
     "data-root":"/var/lib/docker",
-    "insecure-registries":["10.172.49.246"],
+    "insecure-registries":["10.172.49.246", "harbor.dcos.xixian.unicom.local"],
     "registry-mirrors":["https://registry.docker-cn.com", "https://0ea2p7tt.mirror.aliyuncs.com"],
     "log-driver":"json-file",
     "hosts":["tcp://0.0.0.0:2375", "unix:///var/run/docker.sock"],
@@ -55,7 +59,11 @@ chmod a+x /usr/local/bin/docker-compose
 # 个人电脑多保存一些 history
 sed -i 's/HISTSIZE=1000/HISTSIZE=50000/g' /etc/profile
 
+# 个人电脑ssh 存活120分钟, 即2个小时
+sed -i 's/.*ClientAliveInterval.*/ClientAliveInterval 60/' /etc/ssh/sshd_config
+sed -i 's/.*ClientAliveCountMax.*/ClientAliveCountMax 120/' /etc/ssh/sshd_config
+
 # 看使用习惯, 可以不安装, 安装后执行uninstall_oh_my_zsh 卸载
-# use zsh & oh-my-zsh
-yum -y install zsh
+# use zsh & oh-my-zsh (oh-my-zsh need git)
+yum -y install zsh git
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
